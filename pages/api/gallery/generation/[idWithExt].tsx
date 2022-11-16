5aaca5bde5305fe4143117531666e5c4e756edc9
yekta <ngyekta@gmail.com>
2022-11-16 17:04:07 +0300

    Bypass CORS completely

--- pages/api/gallery/generation/[idWithExt].tsx
+++ pages/api/gallery/generation/[idWithExt].tsx
@@ -63,9 +63,6 @@ export default async function handler(req: NextRequest) {
           weight: 800,
         },
       ],
-    }),
-    {
-      origin: "https://stablecog.com",
-    }
+    })
   );
 }
