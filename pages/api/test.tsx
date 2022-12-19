20b56006313cceda693815828b7db5f8e5d9b232
yekta <ngyekta@gmail.com>
2022-12-19 21:12:27 +0300

    Test endpoint

---
+++ pages/api/test.tsx
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import cors from "../../lib/ts/constants/cors";
+
+export const config = {
+  runtime: "experimental-edge",
+};
+
+export default async function handler(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const response = new NextResponse(JSON.stringify(body), {
+      headers: { "Content-Type": "application/json" },
+    });
+    console.log(body);
+    return cors(req, response);
+  } catch (error) {
+    return cors(req, new Response("No body", { status: 500 }));
+  }
+}
