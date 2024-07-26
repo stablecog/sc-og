abc375e37a8286fc7bf0ada1bb73c57d7247698a
yekta <ngyekta@gmail.com>
2024-07-27 00:20:59 +0300

    Fix imports

--- src/ts/constants/fonts.ts
+++ src/ts/constants/fonts.ts
@@ -1,11 +1,20 @@
 export const font400 = fetch(
-  new URL("../../../fonts/avenir-next/avenir-next-400.ttf", import.meta.url)
+  new URL(
+    "../../../assets/fonts/avenir-next/avenir-next-400.ttf",
+    import.meta.url
+  )
 ).then((res) => res.arrayBuffer());
 
 export const font500 = fetch(
-  new URL("../../../fonts/avenir-next/avenir-next-500.ttf", import.meta.url)
+  new URL(
+    "../../../assets/fonts/avenir-next/avenir-next-500.ttf",
+    import.meta.url
+  )
 ).then((res) => res.arrayBuffer());
 
 export const font700 = fetch(
-  new URL("../../../fonts/avenir-next/avenir-next-700.ttf", import.meta.url)
+  new URL(
+    "../../../assets/fonts/avenir-next/avenir-next-700.ttf",
+    import.meta.url
+  )
 ).then((res) => res.arrayBuffer());
