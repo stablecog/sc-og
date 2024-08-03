b6ac627f8f349c0a8e1f3a3995ab70aa6ed00fe6
yekta <ngyekta@gmail.com>
2024-08-03 23:10:12 +0300

    Relative url

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,14 +4,17 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   const { method, url, headers } = request;
   const userAgent = headers.get("user-agent") || "Unknown";
-
-  logger.info(
-    asTable([
-      ["Request Method", method],
-      ["URL", url],
-      ["User Agent", userAgent],
-    ])
-  );
+  const urlObject = new URL(url);
+  const relativeUrl = urlObject.pathname + urlObject.search;
+  if (!userAgent.startsWith("kube-probe")) {
+    logger.info(
+      asTable([
+        ["Request Method", method],
+        ["Relative URL", relativeUrl],
+        ["User Agent", userAgent],
+      ])
+    );
+  }
 
   return NextResponse.next();
 }
